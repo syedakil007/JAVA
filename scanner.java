06b748323e0c8a62e981379b7b6cd63880bb285d
syedakil007 <syedakilmohammed@gmail.com>
2024-09-14 15:09:23 +0530

    initial commit

---
+++ scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class scanner{
+    public static void main(String args[]) 
+    {
+        Scanner john =new Scanner(System.in);
+        int a=john.nextInt();
+        System.out.print(a);        
+    }
+}
